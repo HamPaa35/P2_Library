8e36f0089ffc91d85d979fc8b8c1293ec0b81e5a
Pjevs121 <36846542+Pjevs121@users.noreply.github.com>
2018-03-06 13:21:31 +0100

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,23 @@
+public class Book extends BookShelf {
+	private String name;
+	private String auther;
+	private int id;
+	private int catagory;
+	
+	Book(){
+		
+	}
+	
+	public String getName() {
+		
+	}
+	
+	public String getId() {
+		
+	}
+	
+	public int getCatagory() {
+		
+	}
+	
+}
